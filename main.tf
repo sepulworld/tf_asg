d8a3c829568b5c1d3bfd64ff6339f57ceef3b6e0
zane <zane.williamson@gmail.com>
2016-10-16 17:40:48 -0700

    replacing main

--- main.tf
+++ main.tf
@@ -1,40 +1,52 @@
-resource "aws_elb" "main" {
-  name = "${var.name}"
+resource "aws_launch_configuration" "lc" {
+  lifecycle { create_before_destroy = true }
 
-  internal                  = false
-  cross_zone_load_balancing = true
-  subnets                   = ["${split(",", var.subnet_ids)}"]
-  security_groups           = ["${split(",",var.security_groups)}"]
-
-  idle_timeout                = 30
-  connection_draining         = true
-  connection_draining_timeout = 15
-
-  listener {
-    lb_port            = 443
-    lb_protocol        = "https"
-    instance_port      = "${var.port}"
-    instance_protocol  = "http"
-    ssl_certificate_id = "${var.ssl_certificate_id}"
-  }
-
-  listener {
-    lb_port            = "${var.health_check_port}"
-    lb_protocol        = "http"
-    instance_port      = "${var.health_check_port}"
-    instance_protocol  = "http"
-  }
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
-    target              = "${var.health_check_url}"
-    interval            = 30
-  }
-
-  tags {
-    Name        = "${var.name}-ELB"
-    managed_by  = "terraform"
-  }
+  image_id = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  associate_public_ip_address = "${var.associate_public_ip}"
+  iam_instance_profile = "${var.instance_profile}"
+  key_name = "${var.key_name}"
+  security_groups = ["${var.security_group}"]
+  user_data = "${file(var.user_data)}"
 }
+
+resource "aws_autoscaling_group" "asg" {
+  lifecycle { create_before_destroy = true }
+
+  # We generate a name that includes the launch config name to force a recreate
+  name = "asg-${var.name}-${aws_launch_configuration.lc.name}"
+
+  availability_zones = ["${split(",", var.availability_zones)}"]
+  vpc_zone_identifier = ["${split(",", var.vpc_zone_subnets)}"]
+
+  # Uses the ID from the launch config created above
+  launch_configuration = "${aws_launch_configuration.lc.id}"
+
+  min_size = "${var.asg_min_instances}"
+  max_size = "${var.asg_max_instances}"
+  desired_capacity = "${var.asg_instances}"
+
+  wait_for_elb_capacity = "${var.asg_min_instances}"
+  wait_for_capacity_timeout = "${var.asg_wait_for_capacity_timeout}"
+
+  health_check_grace_period = "${var.health_check_grace_period}"
+  health_check_type = "${var.health_check_type}"
+
+  load_balancers = ["${split(",", var.load_balancer_names)}"]
+
+  tag {
+    key = "Name"
+    value = "asg-${var.name}-${aws_launch_configuration.lc.name}"
+    propagate_at_launch = true
+  }
+  tag {
+    key = "Environment"
+    value = "${var.environment}"
+    propagate_at_launch = true
+  }
+  tag {
+    key = "Team"
+    value = "${var.team}"
+    propagate_at_launch = true
+  }
+
